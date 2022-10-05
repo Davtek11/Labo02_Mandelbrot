cf386b7bd9cbf49fb97c80705c3537acaca033de
david <david.berger@heig-vd.ch>
2022-10-05 17:55:44 +0200

    variable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
+// Florian Duruz & David Berger
+//Labo02_Mandelbrot
+//05.10.2022
+
+
 #include <iostream>
 
+using namespace std;
+
+double x, y, x0, y0;
+int iteration;
+int max_iteration = 1000;
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    cout << "Hello, World!" << endl;
     return 0;
 }
